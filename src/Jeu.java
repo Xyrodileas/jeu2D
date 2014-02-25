d34baae8a0d4ed4ac0cdb551d0b885d0b346ad6b
Xyrodileas <xyrodileas@gmail.com>
2014-02-26 00:08:44 +0100

    Découpage

--- src/Jeu.java
+++ src/Jeu.java
@@ -2,63 +2,70 @@ import java.util.Iterator;
 
 public class Jeu {
 
-  private Integer nbrTours;
+    private Integer nbrTours;
 
     public CollectionJoueur<Joueur> ListeJoueurs = new CollectionJoueur<Joueur>();
 
-  public void calculScoreTours() {
+    public void calculScoreTours() {
 
-      for(Joueur i : ListeJoueurs){
-          int score = ToursBrunco(i);
-          //On va ensuite ajouter le résultat au score du joueur
-          i.ajouterScore(score);
-          //On passe au joueur suivant
+        for(Joueur i : ListeJoueurs){
+            int score = ToursBrunco(i);
+            //On va ensuite ajouter le résultat au score du joueur
+            i.ajouterScore(score);
+            //On passe au joueur suivant
 
-      }
-  }
+        }
+    }
 
-  // Méthode permettant de réaliser un tour de jeu pour un joueur
-  private int ToursBrunco(Joueur leJoueur){
-      int[] resultat = new int[leJoueur.nbrDes()];
-      int index = 0;
-      int score = 0; // Va contenir le score pour le tours du joueur
-      for(int essai = 0; essai < 3; essai++){
-          // On lance chaques dés
-          for(Iterator<De> d = leJoueur.getListeDes().iterator(); d.hasNext(); index++){
-              resultat[index] = d.next().LancerDe();
-          }
+    // Méthode permettant de réaliser un tour de jeu pour un joueur
+    private int ToursBrunco(Joueur leJoueur){
+        int[] resultat;
+        int score = 0; // Va contenir le score pour le tours du joueur
+        for(int essai = 0; essai < 3; essai++){
+            // On lance chaques dés
+            resultat = lancerLesDes(leJoueur);
 
-          // On vérifier les résultats
+            // On vérifier les résultats
 
-          //Si on a 3 nombres égales, on va vérifier si c'est un brunco
-          if(resultat[0] == resultat[1] && resultat[1] == resultat[2]){
-              if(resultat[0] == nbrTours){ // Si c'est un brunco, on passe brunco à vrai
-                  score += 21;
-                  break; // On stop la boucle des essais
-              }
-              else
-                  score += 5;
-          }
-          else{ // Sinon, on vérifie simplement s'il y a des points à ajouer
-              resultatSansBrunco(resultat);
-          }
-      }
-      return score;
-  }
+            //Si on a 3 nombres égales, on va vérifier si c'est un brunco
+            if(resultat[0] == resultat[1] && resultat[1] == resultat[2]){
+                if(resultat[0] == nbrTours){ // Si c'est un brunco, on passe brunco à vrai
+                    score += 21;
+                    break; // On stop la boucle des essais
+                }
+                else
+                    score += 5;
+            }
+            else{ // Sinon, on vérifie simplement s'il y a des points à ajouer
+                resultatSansBrunco(resultat);
+            }
+        }
+        return score;
+    }
 
-  // Méthode vérifiant le nombre de points supplémentaire à ajouter
-  public int resultatSansBrunco(int[] lancers){
-      int score = 0;
-      for(int x : lancers){
-          if(x == nbrTours){
-              score += 1;
-          }
-      }
-      return score;
+    // Permet de réaliser un lancé de chaque dés possédé par le joueur
+    private int[] lancerLesDes(Joueur leJoueur) {
+        int[] resultat = new int[leJoueur.nbrDes()];
+        int index = 0;
+        for(Iterator<De> d = leJoueur.getListeDes().iterator(); d.hasNext(); index++){
+            resultat[index] = d.next().LancerDe();
+        }
+        return resultat;
+    }
+
+    // Méthode vérifiant le nombre de points supplémentaire à ajouter
+    public int resultatSansBrunco(int[] lancers){
+        int score = 0;
+        for(int x : lancers){
+            if(x == nbrTours){
+                score += 1;
+            }
+        }
+        return score;
 
     }
 
-  public void calculerLeVainqueur() {
-  }
+    public void calculerLeVainqueur() {
+    }
 
 }
