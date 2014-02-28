b10f01de11bedf9886feb2d4eb046a335cb460da
i7-9 <i7.aissou@gmail.com>
2014-02-27 19:06:54 -0500

    CollectionDe
    Ajout de l'iterateur et de la méthode hasNext()

--- src/jeu2D/CollectionDes.java
+++ src/jeu2D/CollectionDes.java
@@ -1,50 +1,100 @@
 package jeu2D;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Vector;
 
+
+/**
+ * CollectionDes qui va permettre
+ * de stocker les des des joueurs
+ *
+ */
 public class CollectionDes implements Iterable<De> {
 
-	public Vector<De> ListeDe;
+	public Collection<De> ListeDe;
 	private int nbrDes;
+	private Iterator<De> iterateurDeLaCollection;
 
+	
+	/**
+	 * Constructeur : CollectionDes
+	 * Permet de crÈer une collection vide de des
+	 * et indique le nombre de des=0
+	 */
     public CollectionDes(){
-        ListeDe = new Vector<De>();
+        ListeDe = new ArrayList<De>() ;
+        nbrDes =0;
+        iterateurDeLaCollection=ListeDe.iterator();
+    }
+    
+	
+	/**
+	 * Constructeur : CollectionDes
+	 * Permet de crÈer une collection vide de des
+	 * et indique le nombre de des=0
+	 */
+    public CollectionDes(int nbDe, int nbFaces){
+        ListeDe = new ArrayList<De>() ;
+        nbrDes=nbDe;
+        iterateurDeLaCollection=ListeDe.iterator();
+        for(int i=0;i<nbDe;i++)
+        	ListeDe.add(FabriqueDe.nouveauDe(nbDe, nbFaces)[i]); 
+        	//SI on veux que L'iterateur suive la position de la collection il faut alors dÈcommenter
+        	// iterateurDeLaCollection.next();
     }
 
+    
+    /**
+     * Permet d'ajouter un de ‡ la collection
+     * selon le nombre de face souhaitÈ
+     * @param int face
+     */
 	public void ajouterDe(int faces) { // TODO Modifier pour prendre en compte le nombre de faces
 		ListeDe.add(FabriqueDe.nouveauDe(faces));
-		nbrDes += 1;
+		nbrDes++;
+		 iterateurDeLaCollection.next();
 	}
 
-	@Override
-	public Iterator<De> iterator() { // TODO Impl√©menter l'it√©rateur
-										// correctement
-		return new Iterator<De>() {
-			// Contient l'index de l'√©l√©ment actuel
-			private int currentIndex = 0;
-
-			@Override
-			public boolean hasNext() { // TODO V√©rifier s'il existe un
-										// √©l√©ment suivant
-				return true;
-			}
-
-			@Override
-			public De next() { // TODO Implanter le vrai comportement
-				return FabriqueDe.nouveauDe(5);
-			}
-
-			@Override
-			public void remove() {
-				// TODO Auto-generated method stub
-			}
-
-		};
-	}
-
+	
+	/**
+	 * Permet de retourner
+	 * le nombre de des de la collection
+	 * @return int nombre de des
+	 */
 	public int getNbrDes() {
 		return nbrDes;
 	}
+	
 
+
+	/**
+	 * Permet d'instancier un itÈrateur
+	 * sur la Collection de De
+	 */
+	public Iterator<De> iterator() {
+		return ListeDe.iterator(); 
+
+		}
+	
+	/**
+	 * Permet de savoir si l'element dispose d'un
+	 * de suivant
+	 * @return boolean
+	 */
+	public boolean hasNext(){
+		if(iterateurDeLaCollection.hasNext())
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Methode qui retourne le de 
+	 * de la collection
+	 * @return De
+	 */
+	public De retourneDe(){
+		return null;
+	}
 }
